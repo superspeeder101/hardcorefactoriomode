a83d806d74d9fef3c2b10124aea13e5a5dd3bd5e
superspeeder101 <36306837+superspeeder101@users.noreply.github.com>
2018-11-14 12:09:38 -0800

    Update functions.lua

--- hardcoremodefactorio_0.1.0/util/functions.lua
+++ hardcoremodefactorio_0.1.0/util/functions.lua
@@ -14,4 +14,9 @@ function add_intermediate_product (name, icon, icon_size, subgroup, stacksize)
 end
 
 function add_machine (name, model_table, crafting_speed, ingredient_count, crafting_categories)
+  new_machine = table.deepcopy(model_table)
+  new_machine["name"] = name
+  new_machine["crafting_speed"] = crafting_speed
+  new_machine["ingredient_count"] = ingredient_count
+  
 end
